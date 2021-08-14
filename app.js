126f10692f8d80962c6e29edd4d3b191fdbfa67c
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-08-14 13:10:45 -0300

    Sirvo aplicacion angular

--- app.js
+++ app.js
@@ -15,33 +15,33 @@ hbs.registerPartials(__dirname + '/views/partials')
 // Servir contenido estatico
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-   res.render("Home",{
-      nombre: 'Ivan crack',
-      titulo: 'Curso Node'
-   })
-})
+// app.get('/',(req,res)=>{
+//    res.render("Home",{
+//       nombre: 'Ivan crack',
+//       titulo: 'Curso Node'
+//    })
+// })
 
-app.get('/hola-mundo',(req,res)=>{
-   res.send('Hola mundo desde la ruta')
-})
+// app.get('/hola-mundo',(req,res)=>{
+//    res.send('Hola mundo desde la ruta')
+// })
 
-app.get('/generic',(req,res)=>{
-   res.render('generic',{
-      nombre: 'Ivan crack',
-      titulo: 'Curso Node'
-   })
-})
+// app.get('/generic',(req,res)=>{
+//    res.render('generic',{
+//       nombre: 'Ivan crack',
+//       titulo: 'Curso Node'
+//    })
+// })
 
-app.get('/elements',(req,res)=>{
-   res.render('elements',{
-      nombre: 'Ivan crack',
-      titulo: 'Curso Node'
-   })
-})
+// app.get('/elements',(req,res)=>{
+//    res.render('elements',{
+//       nombre: 'Ivan crack',
+//       titulo: 'Curso Node'
+//    })
+// })
 
 app.get('*',(req,res)=>{
-   res.sendFile( __dirname + '/public/404.html')
+   res.sendFile( __dirname + '/public/index.html')
 })
 
 
